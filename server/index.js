428e784058627c0e5f682492d294b447477af271
Kyrylo <kyryloyunytsky@gmail.com>
2024-03-01 16:03:31 +0200

    be: static files

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.static("public"));
 app.use(cors({origin: "http://localhost:5173", credentials: true}));
 app.use(cookieParser());
 
